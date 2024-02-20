8d42367bef1627a962550cef642c0a92bc4e9db4
listyantidewi1 <dewi@smkn12malang.sch.id>
2024-02-20 08:38:45 +0700

    first commit

---
+++ app/src/main/java/smkn12/tutorials/learnrecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package smkn12.tutorials.learnrecyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
